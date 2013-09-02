5fab49625064c94a9eea77e4e66e817f1af4bbbc
jerome c johnson <jeromecjohnson@Wheezys-Mac-mini.local>
2013-09-02 12:42:41 -0500

    almost done

--- main.rb
+++ main.rb
@@ -45,7 +45,27 @@ helpers do
       end
     end
 
-    "<img src='/images/cards/#{suit}_#{value}.jpg' class='card_image' />"
+    "<img src='/images/cards/#{suit}_#{value}.jpg' class='card_image' style='border:1px solid black'/>"
+  end
+
+  def winner!(msg)
+    @play_again = true
+    @show_buttons = false
+    session[:player_cash] = session[:player_cash] + session[:player_bet]
+    @success = "<strong>#{session[:player_name]} wins</strong> #{msg}"
+  end
+
+  def loser!(msg)
+    @play_again = true
+    @show_buttons = false
+    session[:player_cash] = session[:player_cash] - session[:player_bet]
+    @error = "<strong>#{session[:player_name]} loses!!</strong> #{msg}"
+  end
+
+  def tie!(msg)
+    @play_again = true
+    @show_buttons = false
+    @success = "<strong>Looks like it's a tie!</strong> #{msg}"
   end
 end
 
@@ -62,6 +82,7 @@ get '/' do
 end
 
 get '/new_player' do
+  session[:player_cash] = 2000
   erb :new_player
 end
 
@@ -74,10 +95,35 @@ post '/new_player' do
 
   session[:player_name] = params[:player_name]
   #go to game
-  redirect '/game'
+  redirect '/bet'
+end
+
+get '/bet' do
+  session[:player_bet] = nil
+  erb :bet
+end
+
+post '/bet' do
+  if session[:player_cash] == 0
+    @error = "Oops, you're all out of money! Click 'New Game' to reload!"
+    halt erb(:bet)
+  end
+  if params[:bet_amount].nil? || params[:bet_amount].to_i == 0
+    @error = "Must make a bet."
+    halt erb(:bet)
+  elsif params[:bet_amount].to_i > session[:player_cash]
+    @error = "Bet amount cannot be greater than what you have ($#{session[:player_cash]})"
+    halt erb(:bet)
+  else #happy path
+    session[:player_bet] = params[:bet_amount].to_i
+    redirect '/game'
+  end
 end
 
 get '/game' do
+
+  session[:turn] = session[:player_name]
+
   #set initial values
     #set deck, deal cards
   #render template
@@ -93,32 +139,93 @@ get '/game' do
   session[:dealer_cards] << session[:deck].pop
   session[:player_cards] << session[:deck].pop
 
-
-
   erb :game
 end
 
 
 post '/game/player/hit' do
   session[:player_cards] << session[:deck].pop
+
   player_total = calculate_total(session[:player_cards])
+
   if player_total == 21
-    "Congratulations! You hit Blackjack!!!"
+    winner!("#{session[:player_name]} hit Blackjack!")
+
+    redirect '/game/dealer'
+  elsif player_total == 21 && session[:player_cards].count == 2
+    @success = "Congratulations! You hit Blackjack"
     @show_buttons = false
+    redirect '/game/dealer'
   elsif player_total > 21
-    @error = "Sorry, you busted!!"
-    @show_buttons = false
+    loser!("It looks like you busted at #{player_total}! You lose $#{session[:player_bet]}")
   end
-    erb :game
+
+    erb :game  
 end
 
 post '/game/player/stand' do
   @success = "You have chosen to stand"
   @show_buttons = false
-  erb :game
+  redirect '/game/dealer'
 end
 
 
+get '/game/dealer' do
+  session[:turn] = 'dealer'
+  @show_buttons = false
+
+  dealer_total = calculate_total(session[:dealer_cards])
+
+  if dealer_total == 21
+    loser!("Dealer hit 21! You lose $#{session[:player_bet]}")
+  elsif dealer_total > 21
+    winner!("Dealer busted out at #{dealer_total}!! You win $#{session[:player_bet]}!!")
+  elsif dealer_total >= 17 
+    #dealer stays
+    redirect '/game/who_won'
+  else
+    #dealer hits
+    @show_dealer_hit = true
+  end
+
+  erb :game
+end
+
+post '/game/dealer/hit' do
+  session[:dealer_cards] << session[:deck].pop
+  redirect '/game/dealer'
+end
+
+get '/game/who_won' do
+  @show_buttons = false
+
+  player_total = calculate_total(session[:player_cards])
+  dealer_total = calculate_total(session[:dealer_cards])
+
+  if player_total < dealer_total
+    @error = "Sorry, You Lost $#{session[:player_bet]}!"
+    loser!("You chose to stand at #{player_total} and 
+      the dealer has #{dealer_total}. You lose $#{session[:player_bet]}")
+  elsif player_total > dealer_total
+    winner!("You Win $#{session[:player_bet]}!!")
+  else
+    tie!("Keep your money!")
+  end
+
+  erb :game
+end
+
+get '/game_over' do
+  erb :game_over
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/rules' do
+  erb :rules
+end
 
 
 
